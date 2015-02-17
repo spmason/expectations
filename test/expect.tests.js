7e08bb83048c27db42733eaec3b20ac21b2c7f34
Francesco Trotta <>
2015-02-17 01:59:48 +0100

    Fixed test failure for time zone other than GMT

--- test/expect.tests.js
+++ test/expect.tests.js
@@ -494,7 +494,7 @@
             });
             it('can generate correct message for Dates', function(){
                 try{
-                    expect(new Date(2012, 0, 1)).not.toBeDefined();
+                    expect(new Date(Date.UTC(2012, 0, 1))).not.toBeDefined();
                 }catch(err){
                     if(err.message !== 'expected [Date Sun, 01 Jan 2012 00:00:00 GMT] not to be defined'){
                         throw new Error('Expected error message is not correct: ' + err.message);
