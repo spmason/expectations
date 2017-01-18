323e11d1533f327a0637140d6e449b7feb46baf5
Steve Mason <steve@spmason.com>
2017-01-18 21:15:22 +0000

    Fixed eslint problems

--- expectations.js
+++ expectations.js
@@ -119,33 +119,33 @@
         var className = toString.call(a);
         if (className != toString.call(b)) return false;
         switch (className) {
-              // Strings, numbers, dates, and booleans are compared by value.
-            case '[object String]':
-              // Primitives and their corresponding object wrappers are equivalent; thus, `"5"` is
-              // equivalent to `new String("5")`.
-              return a == String(b);
-            case '[object Number]':
-              // `NaN`s are equivalent, but non-reflexive. An `egal` comparison is performed for
-              // other numeric values.
-              return a != +a ? b != +b : (a ? 1 / a == 1 / b : a == +b);
-            case '[object Date]':
-            case '[object Boolean]':
-              // Coerce dates and booleans to numeric primitive values. Dates are compared by their
-              // millisecond representations. Note that invalid dates with millisecond representations
-              // of `NaN` are not equivalent.
-              return +a == +b;
-              // RegExps are compared by their source patterns and flags.
-            case '[object RegExp]':
-              return a.source == b.source && a.global == b.global && a.multiline == b.multiline && a.ignoreCase == b.ignoreCase;
+        // Strings, numbers, dates, and booleans are compared by value.
+        case '[object String]':
+            // Primitives and their corresponding object wrappers are equivalent; thus, `"5"` is
+            // equivalent to `new String("5")`.
+            return a == String(b);
+        case '[object Number]':
+            // `NaN`s are equivalent, but non-reflexive. An `egal` comparison is performed for
+            // other numeric values.
+            return a != +a ? b != +b : (a ? 1 / a == 1 / b : a == +b);
+        case '[object Date]':
+        case '[object Boolean]':
+            // Coerce dates and booleans to numeric primitive values. Dates are compared by their
+            // millisecond representations. Note that invalid dates with millisecond representations
+            // of `NaN` are not equivalent.
+            return +a == +b;
+            // RegExps are compared by their source patterns and flags.
+        case '[object RegExp]':
+            return a.source == b.source && a.global == b.global && a.multiline == b.multiline && a.ignoreCase == b.ignoreCase;
         }
         if (typeof a != 'object' || typeof b != 'object') return false;
         // Assume equality for cyclic structures. The algorithm for detecting cyclic
         // structures is adapted from ES 5.1 section 15.12.3, abstract operation `JO`.
         var length = stack.length;
         while (length--) {
-          // Linear search. Performance is inversely proportional to the number of
-          // unique nested structures.
-          if (stack[length] == a) return true;
+            // Linear search. Performance is inversely proportional to the number of
+            // unique nested structures.
+            if (stack[length] == a) return true;
         }
         // Add the first object to the stack of traversed objects.
         stack.push(a);
@@ -153,43 +153,42 @@
             result = true;
         // Recursively compare objects and arrays.
         if (arrayClassLookup.indexOf(className) > -1) {
-          // Compare array lengths to determine if a deep comparison is necessary.
-          size = a.length;
-          result = size == b.length;
-          if (result) {
-            // Deep compare the contents, ignoring non-numeric properties.
-            while (size--) {
-              // Ensure commutative equality for sparse arrays.
-              if (!(result = size in a == size in b && eq(a[size], b[size], stack))) break;
+            // Compare array lengths to determine if a deep comparison is necessary.
+            size = a.length;
+            result = size == b.length;
+            if (result) {
+                // Deep compare the contents, ignoring non-numeric properties.
+                while (size--) {
+                    // Ensure commutative equality for sparse arrays.
+                    if (!(result = size in a == size in b && eq(a[size], b[size], stack))) break;
+                }
             }
-          }
         } else {
-          // Objects with different constructors are not equivalent.
-          if ('constructor' in a != 'constructor' in b || a.constructor != b.constructor) return false;
-          // Deep compare objects.
-          for (var key in a) {
-            if (hasOwnProperty.call(a, key) && a[key] !== undefined) {
-              // Count the expected number of properties.
-              size++;
-              // Deep compare each member.
-              if (!(result = hasOwnProperty.call(b, key) && eq(a[key], b[key], stack))) break;
+            // Objects with different constructors are not equivalent.
+            if ('constructor' in a != 'constructor' in b || a.constructor != b.constructor) return false;
+            // Deep compare objects.
+            for (var key in a) {
+                if (hasOwnProperty.call(a, key) && a[key] !== undefined) {
+                    // Count the expected number of properties.
+                    size++;
+                    // Deep compare each member.
+                    if (!(result = hasOwnProperty.call(b, key) && eq(a[key], b[key], stack))) break;
+                }
             }
-          }
-          // Ensure that both objects contain the same number of properties.
-          if (result) {
-            for (key in b) {
-              if (hasOwnProperty.call(b, key) && b[key] !== undefined && !(size--)) break;
+            // Ensure that both objects contain the same number of properties.
+            if (result) {
+                for (key in b) {
+                    if (hasOwnProperty.call(b, key) && b[key] !== undefined && !(size--)) break;
+                }
+                result = !size;
             }
-            result = !size;
-          }
         }
         // Remove the first object from the stack of traversed objects.
         stack.pop();
         return result;
-      }
+    }
 
     function Expect(value, assertions, expr, parent){
-        var self = this;
         expr = expr || '';
 
         this.value = value;
@@ -255,7 +254,7 @@
     Expect.prototype.toBeTruthy = function(customMsg){
         var generateMessage = this.generateMessage(this.value, this.expr, 'to be truthy', undefined, customMsg);
 
-        if(!!this.value){
+        if(this.value){
             return this.pass(generateMessage);
         }
         this.fail(generateMessage);
